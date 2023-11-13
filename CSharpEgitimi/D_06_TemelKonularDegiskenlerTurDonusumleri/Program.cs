c5f28d40e6e2711b39f491edac4161b2e8613c57
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2023-11-13 20:05:43 +0300

    D 06 V27 Char değişken tipi
    Video 27

--- CSharpEgitimi/D_06_TemelKonularDegiskenlerTurDonusumleri/Program.cs
+++ CSharpEgitimi/D_06_TemelKonularDegiskenlerTurDonusumleri/Program.cs
@@ -19,39 +19,50 @@ namespace D_06_TemelKonularDegiskenlerTurDonusumleri
         #endregion
 
         #region VİDEO 26
+        //static void Main(string[] args)
+        //{
+        //    string isimSoyisim1 = "Mücahit Çetinkaya1";
+        //    Console.WriteLine(isimSoyisim1);
+
+
+        //    isimSoyisim1 = "Muhammet Çetinkaya2";
+        //    Console.WriteLine(isimSoyisim1);
+
+        //    /* büyük karakter küçük karakter */
+
+        //    string isimSoyisim2 = "Mücahit Çetinkaya3";
+        //    string buyukKarakter1 = isimSoyisim2.ToUpper();
+        //    string kucukkKarakter1 = isimSoyisim2.ToLower();
+        //    Console.WriteLine(buyukKarakter1);
+        //    Console.WriteLine(kucukkKarakter1);
+
+        //    /* belirli bir bölümü vermiş oldugum kısımı console a yazdırır */
+
+        //    /* Substring */
+
+        //    string isimSoyisim3 = "Mü4444t Çetinkaya4";
+        //    string belirliBirBolum1 = isimSoyisim3.Substring(2, 4);
+        //    Console.WriteLine(belirliBirBolum1);
+
+
+        //    string isimSoyisim4 = "Mü5555t Çetinkaya5";
+        //    string buyukKarakter2 = isimSoyisim4.ToUpper();
+        //    Console.WriteLine(buyukKarakter2);
+        //    string kucukkKarakter2 = isimSoyisim4.ToLower();
+        //    Console.WriteLine(kucukkKarakter2);
+        //    string belirliBirBolum2 = isimSoyisim4.Substring(2, 4);
+        //    Console.WriteLine(belirliBirBolum2);
+        //    Console.ReadLine();
+        //}
+        #endregion
+
+        #region VİDEO 27
         static void Main(string[] args)
         {
-            string isimSoyisim1 = "Mücahit Çetinkaya1";
-            Console.WriteLine(isimSoyisim1);
-
-
-            isimSoyisim1 = "Muhammet Çetinkaya2";
-            Console.WriteLine(isimSoyisim1);
-
-            /* büyük karakter küçük karakter */
-
-            string isimSoyisim2 = "Mücahit Çetinkaya3";
-            string buyukKarakter1 = isimSoyisim2.ToUpper();
-            string kucukkKarakter1 = isimSoyisim2.ToLower();
-            Console.WriteLine(buyukKarakter1);
-            Console.WriteLine(kucukkKarakter1);
-
-            /* belirli bir bölümü vermiş oldugum kısımı console a yazdırır */
-
-            /* Substring */
-
-            string isimSoyisim3 = "Mü4444t Çetinkaya4";
-            string belirliBirBolum1 = isimSoyisim3.Substring(2, 4);
-            Console.WriteLine(belirliBirBolum1);
-
-
-            string isimSoyisim4 = "Mü5555t Çetinkaya5";
-            string buyukKarakter2 = isimSoyisim4.ToUpper();
-            Console.WriteLine(buyukKarakter2);
-            string kucukkKarakter2 = isimSoyisim4.ToLower();
-            Console.WriteLine(kucukkKarakter2);
-            string belirliBirBolum2 = isimSoyisim4.Substring(2, 4);
-            Console.WriteLine(belirliBirBolum2);
+            string kullaniciSecimString = "A";
+            Console.WriteLine(kullaniciSecimString);
+            char kullaniciSecimChar = 'A';
+            Console.WriteLine(kullaniciSecimChar);
             Console.ReadLine();
         }
         #endregion
