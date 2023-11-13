45289b9443f173fe6c5c2309569ca3bccb89b484
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2023-11-13 20:04:10 +0300

    D 06 V26 String değişken tipi
    Video 26

--- CSharpEgitimi/D_06_TemelKonularDegiskenlerTurDonusumleri/Program.cs
+++ CSharpEgitimi/D_06_TemelKonularDegiskenlerTurDonusumleri/Program.cs
@@ -9,13 +9,52 @@ namespace D_06_TemelKonularDegiskenlerTurDonusumleri
     internal class Program
     {
         #region VİDEO 25
+        //static void Main(string[] args)
+        //{
+        //    /* degiskenTipi degiskenAdi = değer; */
+        //    string isimSoyisim = "Mücahit Çetinkaya";
+        //    Console.WriteLine(isimSoyisim);
+        //    Console.ReadLine();
+        //}
+        #endregion
+
+        #region VİDEO 26
         static void Main(string[] args)
         {
-            /* degiskenTipi degiskenAdi = değer; */
-            string isimSoyisim = "Mücahit Çetinkaya";
-            Console.WriteLine(isimSoyisim);
+            string isimSoyisim1 = "Mücahit Çetinkaya1";
+            Console.WriteLine(isimSoyisim1);
+
+
+            isimSoyisim1 = "Muhammet Çetinkaya2";
+            Console.WriteLine(isimSoyisim1);
+
+            /* büyük karakter küçük karakter */
+
+            string isimSoyisim2 = "Mücahit Çetinkaya3";
+            string buyukKarakter1 = isimSoyisim2.ToUpper();
+            string kucukkKarakter1 = isimSoyisim2.ToLower();
+            Console.WriteLine(buyukKarakter1);
+            Console.WriteLine(kucukkKarakter1);
+
+            /* belirli bir bölümü vermiş oldugum kısımı console a yazdırır */
+
+            /* Substring */
+
+            string isimSoyisim3 = "Mü4444t Çetinkaya4";
+            string belirliBirBolum1 = isimSoyisim3.Substring(2, 4);
+            Console.WriteLine(belirliBirBolum1);
+
+
+            string isimSoyisim4 = "Mü5555t Çetinkaya5";
+            string buyukKarakter2 = isimSoyisim4.ToUpper();
+            Console.WriteLine(buyukKarakter2);
+            string kucukkKarakter2 = isimSoyisim4.ToLower();
+            Console.WriteLine(kucukkKarakter2);
+            string belirliBirBolum2 = isimSoyisim4.Substring(2, 4);
+            Console.WriteLine(belirliBirBolum2);
             Console.ReadLine();
         }
         #endregion
+
     }
 }
