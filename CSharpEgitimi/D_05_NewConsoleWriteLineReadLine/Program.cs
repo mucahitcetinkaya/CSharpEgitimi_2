f6f6e4ad99afb2b55cce508da30158b984ebd192
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2023-11-13 19:24:37 +0300

    D 05 Console Write WriteLine Read ReadLine

---
+++ CSharpEgitimi/D_05_NewConsoleWriteLineReadLine/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_05_NewConsoleWriteLineReadLine
+{
+    internal class Program
+    {
+        // VİDEO 21 Console.Write ve Console.WriteLine komutları
+
+        //static void Main(string[] args)
+        //{
+
+        //    Console.Write("Merhaba Dünya ");
+        //    Console.Write("Ben C# Öğreniyorum");
+
+        //    Console.WriteLine(" ");
+
+        //    Console.WriteLine("Merhaba Dünya");
+        //    Console.WriteLine("Ben C# Öğreniyorum");
+
+        //    Console.ReadLine();
+        //}
+
+        // VİDEO 23 Uygulamaları satır , satır inceleme
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1.Satır");
+            Console.WriteLine("2.Satır");
+            Console.WriteLine("3.Satır");
+            Console.WriteLine("4.Satır");
+            Console.WriteLine("5.Satır");
+            Console.ReadLine();
+        }
+    }
+}
