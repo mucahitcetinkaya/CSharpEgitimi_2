8d31ad346d62d18796f2a1a3c43321a8be8fd51f
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2023-11-15 17:12:59 +0300

    D 06 V40 Convert ve Parse Kullanımları

--- CSharpEgitimi/D_06_TemelKonularDegiskenlerTurDonusumleri/Program.cs
+++ CSharpEgitimi/D_06_TemelKonularDegiskenlerTurDonusumleri/Program.cs
@@ -364,10 +364,34 @@ namespace D_06_TemelKonularDegiskenlerTurDonusumleri
         #endregion
 
         #region VİDEO 39
+        //static void Main(string[] args)
+        //{
+        //    object o1 = 100;
+        //    object o2 = 10.90;
+        //}
+        #endregion
+
+        #region VİDEO 40
         static void Main(string[] args)
         {
-            object o1 = 100;
-            object o2 = 10.90;
+            string metin1 = "100";
+            int s1 = metin1;
+
+            int s2 = (int)metin1;
+
+            int s3 = int.Parse(metin1);
+            int s4 = Convert.ToInt32(metin1);
+
+            string metin2 = "True";
+            bool b1 = bool.Parse(metin2);
+            bool b2 = Convert.ToBoolean(metin2);
+
+            string metin3 = null;
+
+            decimal d1 = decimal.Parse(metin3);
+            decimal d2 = Convert.ToDecimal(metin3);
+
+            bool b3 = Convert.ToBoolean(metin3);
         }
         #endregion
     }
