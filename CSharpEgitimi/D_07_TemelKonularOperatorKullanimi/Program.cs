f75f456b135797ec8e0a3c77010b5fec10416729
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2023-11-17 19:38:49 +0300

    D 07 V45 Toplama

--- CSharpEgitimi/D_07_TemelKonularOperatorKullanimi/Program.cs
+++ CSharpEgitimi/D_07_TemelKonularOperatorKullanimi/Program.cs
@@ -8,9 +8,74 @@ namespace D_07_TemelKonularOperatorKullanimi
 {
     internal class Program
     {
+        //#region ÖRNEK
+        //static void Main(string[] args)
+        //{
+
+        //}
+        //#endregion
+
+        #region VİDEO 45
         static void Main(string[] args)
         {
 
+            /* int +int(int verir) */
+
+            int s1 = 10;
+            int s2 = 15;
+            int toplamInt = s1 + s2;
+
+            /*
+             burada s1 s2 değişken tipine bakıyor int görüyor
+             ikiside int oldugundan + operatörü toplama işlemi yapıp
+             toplamInt üzerine alır
+             
+            */
+
+            /* byte +byte(int verir) */
+            byte s3 = 10;
+            byte s4 = 10;
+            byte toplamByte1 = s3 + s4;
+
+            /*
+             burada en sonda byte verirsek hata veriyor program
+             sebebi byte aralıgı düşük byte ı geçme ihtimali var
+             1 ile 10 da olabilir 255 ile 1 de olabilir 255 i geçme ihtimali var
+             sen oraya int ver diyor
+             aynısını int e cevirirsek hata vermiyor
+            */
+
+            byte s5 = 10;
+            byte s6 = 10;
+            int toplamByte2 = s5 + s6;
+            /* int yapınca hata vermiyor var da yazabiliriz int oldugunu anlar */
+
+            /* string +int(string verir) */
+
+            string metin1 = "Merhaba";
+            int s7 = 10;
+            string toplamString1 = metin1 + s5; /* değişken adı direk yazılır */
+
+            /*
+             bunun cıktısı Merhaba 10 olarak olur yanına birleştirme yapar
+             ben merhaba ile 10 u toplayamam birleştirme yapayım diyor
+             int +string(string verir)
+            */
+
+            string toplamString2 = s5 + metin1;
+
+            /* 
+             bunun cıktısı 10Merhaba olarak olur yanına birleştirme yapar
+             string +string(string verir)
+            */
+
+            string metin2 = "Dünya";
+            string toplamString3 = metin1 + metin2;
+
+            /* bunun cıktısı MMerhabaDünya olarak olur yanına birleştirme yapar */
+
         }
+        #endregion
+
     }
 }
