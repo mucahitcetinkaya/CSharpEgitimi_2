2ff3031b1d16c57f4acb27f36c4f6aeb30a82401
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2023-11-30 19:25:00 +0300

    D 08 V64-V65 Temel Konular Koşul İfadeleri
    Video 64 ders 8 içerikleri
    Video 65 Karar Yapıları
    Algoritma nedir ?

---
+++ CSharpEgitimi/D_08_TemelKonularKosulIfadeleri/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_08_TemelKonularKosulIfadeleri
+{
+    internal class Program
+    {
+        //#region VİDEO 
+        //static void Main(string[] args)
+        //{
+
+        //}
+        //#endregion
+
+
+        #region VİDEO 
+        static void Main(string[] args)
+        {
+
+        }
+        #endregion
+    }
+}
