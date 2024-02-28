6828a465890af0a3577da2e0c6899e5f472934f7
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-02-28 21:43:39 +0300

    D 09 V76 For Döngüsü

--- CSharpEgitimi/D_09_TemelKonularDonguler/Program.cs
+++ CSharpEgitimi/D_09_TemelKonularDonguler/Program.cs
@@ -17,6 +17,37 @@ namespace D_09_TemelKonularDonguler
         //}
         //#endregion
 
+        #region VİDEO 76
+
+        //static void Main(string[] args)
+        //{
+
+        //    /*
+        //    for döngüsü oluşturalım
+        //    for yazıp tab tab yapınca otomatik olusturdu
+        //    int değişken tipinde i adında içerisinde 0 değeri olan sayaç kısmı
+        //    0 default değeri saymaya 0 dan başla demiş oluyoruz.
+        //    i değişkeni 10 dan kucuk yada eşitse bu işlemi yap diyoruz şart kısmı
+        //    program buraya kadar geliyor kucuk yada eşit mi diye bakar true ise içindeki işlemi yapar
+        //    işlemi yaptıktan sonra i++ kısmı sayaç artış kısmı oluyor biz burda++ ile 1 arttır demişiz
+        //    illa++ veya 1 arttır dememiz gerekmiyor istediğimizi yazabiliriz.
+        //    sayaç artış kısmını okur tekrar eder şart sağlana kadar
+        //    şartımız 10 dan kucuk yada eşit 10 a eşit olunca yine yazdırır
+        //    11 olunca false verir yazdırmaz programı durdurur.
+        //    */
+
+        //    for (int i = 0; i <= 10; i++)
+        //    {
+        //        Console.WriteLine("Ben bir for döngüsüyüm.");
+        //    }
+
+        //    Console.ReadLine();
+
+        //}
+
+        #endregion
+
+
 
     }
 }
