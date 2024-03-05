1a0e629069a63f27058464a443123e83394e5906
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-03-05 20:42:48 +0300

    D 10 V92 Diziler Arrays Dizi Nedir

--- CSharpEgitimi/D_10_Diziler-Arrays/Program.cs
+++ CSharpEgitimi/D_10_Diziler-Arrays/Program.cs
@@ -18,6 +18,54 @@ namespace D_10_Diziler_Arrays
 
         //#endregion
 
+        #region VİDEO 92
+
+        //static void Main(string[] args)
+        //{
+
+        //    /*
+
+        //    Bölüm 10 Diziler ( Arrays ) VİDEO 92 Dizi Nedir ?
+
+        //    */
+
+        //    /*
+            
+        //    Anlatımı notlarda var
+
+        //    */
+
+        //    string[] ogrenciIsimListesi = new string[5];
+        //    ogrenciIsimListesi[0] = "Muhammet";
+        //    ogrenciIsimListesi[1] = "Mücahit";
+        //    ogrenciIsimListesi[2] = "Çetinkaya";
+        //    ogrenciIsimListesi[3] = "Arden";
+        //    ogrenciIsimListesi[4] = "Zayn";
+
+        //    Console.WriteLine("foreach ile olan");
+
+        //    foreach (string item in ogrenciIsimListesi)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+        //    Console.WriteLine();
+        //    Console.WriteLine("for ile olan");
+
+        //    for (int i = 0; i < ogrenciIsimListesi.Length; i++)
+        //    {
+        //        string gelenIsim = ogrenciIsimListesi[i];
+        //        Console.WriteLine(gelenIsim);
+        //    }
+
+        //    Console.ReadLine();
+
+        //}
+
+        #endregion
+
+
+
 
 
     }
