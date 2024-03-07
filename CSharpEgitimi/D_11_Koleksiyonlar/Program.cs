19aba385849483eac112c3400bcea6821e44091e
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-03-07 22:03:13 +0300

    D 11 Koleksiyonlar V95 Bilgilendirme

---
+++ CSharpEgitimi/D_11_Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_11_Koleksiyonlar
+{
+    internal class Program
+    {
+
+        //#region VİDEO 
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        //#endregion
+
+
+
+
+
+
+    }
+}
