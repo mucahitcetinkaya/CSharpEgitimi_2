9c034e20f2492a9d878cb6cbb2ff60a6141785da
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-04-02 23:58:52 +0300

    D 12 Metot V118 Metot içinde metot tanımlama ( Local function )

---
+++ CSharpEgitimi/D_12_Metot/Deneme118.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace D_12_Metot
+//{
+//    public class Deneme118
+//    {
+//        public static void Selam()
+//        {
+//            Console.WriteLine("static metot newlemeden calıstı");
+//        }
+//    }
+//}
+
+///*
+
+//public class içinde
+//puclic static bir metot olusturduk
+ 
+ 
+//*/
