f0d6342339cdc9ff6b8d324b123d20dc53e80463
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-04-09 11:32:32 +0300

    D 13 OOP Sınıflar V120 Bilgilendirme

---
+++ CSharpEgitimi/D_13_OOP_Siniflar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_13_OOP_Siniflar
+{
+    internal class Program
+    {
+
+        //#region VİDEO 
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        //#endregion
+
+        #region VİDEO 120
+
+        //static void Main(string[] args)
+        //{
+
+        //    /*
+             
+        //    Bölüm 13 OOP Sınıflar VİDEO 120 Bilgilendirme
+             
+        //    */
+
+        //    /*
+            
+        //    notlarda acıklaması var
+             
+        //    */
+
+
+        //}
+
+        #endregion
+
+
+
+
+
+    }
+}
