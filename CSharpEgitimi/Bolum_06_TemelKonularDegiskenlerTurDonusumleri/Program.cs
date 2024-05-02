ae415d2cfbc1b83ece01d3c27ed55b9b3d7056bb
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-05-02 23:11:48 +0300

    Bolum 06 Temel Konular - Degiskenler ve Tur Donusumleri V25 Bilgilendirme

---
+++ CSharpEgitimi/Bolum_06_TemelKonularDegiskenlerTurDonusumleri/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Bolum_06_TemelKonularDegiskenlerTurDonusumleri
+{
+    internal class Program
+    {
+
+        //#region VİDEO 
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        //#endregion
+
+        #region VİDEO 
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        #endregion
+
+    }
+}
