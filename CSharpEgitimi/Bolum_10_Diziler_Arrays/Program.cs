f6e0f79e297c109511a13495933717eeaba36f0a
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-05-15 22:57:39 +0300

    Bolum 10 Diziler Arrays V92 Bilgilendirme

---
+++ CSharpEgitimi/Bolum_10_Diziler_Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Bolum_10_Diziler_Arrays
+{
+    internal class Program
+    {
+
+        //#region VİDEO 
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        //#endregion
+
+        #region VİDEO 92 Bilgilendirme
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        #endregion
+
+
+
+
+
+
+
+
+      
+    }
+}
--- CSharpEgitimi/Bolum_10_Diziler_Arrays/Properties/AssemblyInfo.cs
+++ CSharpEgitimi/Bolum_10_Diziler_Arrays/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("D_09_TemelKonularDonguler")]
+[assembly: AssemblyTitle("Bolum_10_Diziler_Arrays")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("D_09_TemelKonularDonguler")]
+[assembly: AssemblyProduct("Bolum_10_Diziler_Arrays")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("45b8260e-df56-42b7-a42c-a4747fac28f0")]
+[assembly: Guid("30b9ce2a-4085-4b59-8db6-dcaea59efcf0")]
 
 // Version information for an assembly consists of the following four values:
 //
