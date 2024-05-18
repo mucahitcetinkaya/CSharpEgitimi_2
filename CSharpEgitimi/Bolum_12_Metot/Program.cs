c23992ec0940590d7d8d3f1503d59e2b6a33420f
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-05-18 23:54:13 +0300

    Bolum 12 Metot V106 Main Metodu İnceleme

--- CSharpEgitimi/Bolum_12_Metot/Program.cs
+++ CSharpEgitimi/Bolum_12_Metot/Program.cs
@@ -21,8 +21,43 @@
 
         #endregion
 
+        #region VİDEO 106 Main Metotu İnceleme
 
+        //static void Main(string[] args)
+        //{
+        //    /*
 
+        //    Bölüm 12 Metot VİDEO 106 Main Metotu İnceleme 
+
+        //    */
+
+        //    /*
+
+        //    notlarda tamamı var
+
+        //    */
+
+        //    Console.WriteLine("Merhaba");
+
+        //    System.Console.WriteLine("Merhaba");
+
+        //    Selamla();
+
+        //    Console.ReadLine();
+
+        //}
+
+        ///*
+
+        //anlatımı notlarda yazıyor
+
+        //*/
+        //static void Selamla()
+        //{
+        //    Console.WriteLine("Merhaba Metot");
+        //}
+
+        #endregion
 
 
 
