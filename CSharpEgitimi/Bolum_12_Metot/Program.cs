9f1d620daadfad75cc240e57836af54dba12cc62
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-05-19 00:02:20 +0300

    Bolum 12 Metot V107 C# Nesne Yönelimli Programlama ( Kısa Giriş )

--- CSharpEgitimi/Bolum_12_Metot/Program.cs
+++ CSharpEgitimi/Bolum_12_Metot/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Bolum_12_Metot
+﻿//using D_12_Metot;
+
+namespace Bolum_12_Metot
 {
     internal class Program
     {
@@ -59,6 +61,71 @@
 
         #endregion
 
+        #region VİDEO 107 C# Nesne Yönelimli Programlama ( Kısa Giriş )
+
+        //static void Main(string[] args)
+        //{
+
+        //    /*
+
+        //    Bölüm 12 Metot VİDEO 107 C# Nesne yönelimli programlama ( Kısa Giriş )
+
+        //    */
+
+        //    /*
+
+        //    Metotlar kısmına calısmak için yeni bir class olusturup
+        //    bu class ı ornekleyip metotlarına erişip main metotunda metotların tanımlarını incelememiz lazım
+
+        //    şimdilik class kavramına girmiyorum ihtiyacımız oldugu kadar anlatacagım
+
+        //    yeni bir class eklemek için
+        //    Solution Explorer kısmında ki uygulamamızın üstüne gelelim calısıtıgımız projeye
+        //    D_12_Metot sag tık Add e tıklayıp Class kısmını secelim
+        //    acılan pencerede class secip isim verelim 
+        //    Ogrenci adını verdik
+        //    uygulamamızın içerisinde bir Ogrenci.cs adında bir dosya olustu 
+        //    Ogrenci class ımız olusmus oldu
+
+        //    Ogrenci.cs de devam edelim
+
+        //    Ogrenci.cs den geldik
+
+        //    Ogrenci class ını cagıralım burda
+
+        //    class kullandıgımız için new lemek orneklemek instance olusturmak gerekiyor
+        //    burda sadece class a ulasmak için olarak dusunebilirsin 
+        //    aslında arka tarafta stack heap yapıları bişeyler degişiyor ama class bölümünde göreceğiz
+
+
+        //                Ogrenci107 O1 = new Ogrenci107();
+
+        //    şimdi O1. dedigimizde içindeki metoda ulasmıs olduk
+
+        //    O1.OgrenciSelamla();
+
+        //    metotu yazarken parantezlere geldiğinde tooltype cıkıyor 
+        //    void Ogrenci.OgrenciSelamla() yazıyor
+        //    yani parametre almamıs geriye bişey döndürmüyor bilgisini veriyor
+
+        //                Ogrenci107 O1 = new Ogrenci107();
+
+        //    e gelip breakpoint koyup debug edelim
+
+        //    f11 ile devam ediyoruz
+        //    nesneyi olusturdu o1. kısmında metotun oldugu yere gitti Ogrenci class ına 
+        //    OgrenciSelamla metodunun scoplarına düştü
+        //    calıstırdı bitirdi geriye buraya döndü 
+
+        //    */
+
+        //    Ogrenci107 O1 = new Ogrenci107();
+        //    O1.OgrenciSelamla();
+
+
+        //}
+
+        #endregion
 
 
 
