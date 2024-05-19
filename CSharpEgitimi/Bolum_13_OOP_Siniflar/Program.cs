a463c89120bdf58f69a81bae8eb947692a9ebab5
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-05-19 16:53:27 +0300

    Bolum 13 OOP Siniflar V121 Bilgilendirme

---
+++ CSharpEgitimi/Bolum_13_OOP_Siniflar/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Bolum_13_OOP_Siniflar
+{
+    internal class Program
+    {
+
+        //#region VİDEO 
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        //#endregion
+
+        #region VİDEO 121 Bilgilendirme
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- CSharpEgitimi/Bolum_13_OOP_Siniflar/Properties/AssemblyInfo.cs
+++ CSharpEgitimi/Bolum_13_OOP_Siniflar/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("D_12_Metot")]
+[assembly: AssemblyTitle("Bolum_13_OOP_Siniflar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("D_12_Metot")]
+[assembly: AssemblyProduct("Bolum_13_OOP_Siniflar")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9e2c09fb-7e14-404b-9e52-c8112ba29ea9")]
+[assembly: Guid("21530a77-347f-403b-8772-218fc7010a87")]
 
 // Version information for an assembly consists of the following four values:
 //
