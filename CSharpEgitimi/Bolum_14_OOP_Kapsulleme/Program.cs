1dafb8e8df97d9f917c01d399e990e973d32fdbc
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-06-16 19:25:19 +0300

    Bolum 14 OOP Kapsulleme V127 Bilgilendirme

---
+++ CSharpEgitimi/Bolum_14_OOP_Kapsulleme/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Bolum_14_OOP_Kapsulleme
+{
+    internal class Program
+    {
+
+        //#region VİDEO 
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        //#endregion
+
+        #region VİDEO 127 Bilgilendirme
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        #endregion
+
+
+
+
+
+    }
+}
