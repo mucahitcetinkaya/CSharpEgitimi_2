4650c44a818db6fe2d58b1bd0ce5c32e03dc4d85
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-06-23 18:42:30 +0300

    Bolum 15 OOP Static Kavrami V133 Static Nedir ?

---
+++ CSharpEgitimi/Bolum_15_OOP_Static_Kavrami/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace Bolum_15_OOP_Static_Kavrami
+{
+    internal class Program
+    {
+
+        //#region VİDEO 
+
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        //#endregion
+
+        #region VİDEO 133 Static Nedir ?
+
+        //static void Main(string[] args)
+        //{
+
+        //    /*
+
+        //    Bolum 15 OOP Static Kavrami V133 Static Nedir ?
+
+        //    */
+
+        //    /*
+            
+        //    OOP - Static
+
+        //    Nesne Yönelimli Programlama - Static Kavramı
+
+        //    biz bu bolume kadar neler yaptık
+        //    bir nesne olusturduk
+        //    nesnemizi new ile ornekledıgımız de 
+        //    onun steak (pointer) ve heap tarafında bir bölüm oldugunu gördük
+        //    referans tipli davranısını inceledik
+
+        //    daha sonra sınıf içerisinde olusturmus oldugumuz alanlarımızı
+        //    nasıl özelleştirebileceğimizi inceledik
+
+        //    şimdi ram kısmını cizerken hatırlasınız ki burayı biraz daha sonra anlatacagım
+
+        //    o kısma geldik
+
+        //            STEAK                   HEAP
+        //    ----------------------------------------------
+        //    I                     I                      I
+        //    I                     I                      I
+        //    I                     I                      I
+        //    I                     I                      I
+        //    ----------------------------------------------
+        //    I                                            I
+        //    I                                            I
+        //    ----------------------------------------------
+        //                        STATİC
+             
+
+        //    nesne üzerinde static nedir
+        //    static içerisinde olusturdugumuz 
+        //    field lar metotlar nelerdir
+
+        //    Nesne Yönelimli Programlama konusunda önemli bir
+        //    bölüm olan static konusuna geldik
+
+        //    STATİC Class - Field - Metot
+
+        //    Bölüm içerisinde
+
+        //    Static Nedir ?
+        //    Statik ram bölgesindeki yeri
+
+        //    Static class nedir
+        //        Static yapıcı metot nedir ?
+        //        Standart yapıcı metot'dan farkı nedir
+
+        //    Static field nedir nasıl ulasırız default deger nası atarız
+
+        //    Static metot nedir
+
+        //    Konularını örnekler ile inceliyor olacagız
+
+        //    bir önceki orneklerımız de kapsulleme de 
+        //    her bir string içerisinde dönerken
+        //    char ın statik olan bir metotundan faydalanmıstık
+        //    IsNumber
+        //    .net framework un de kullandıgı gibi bizde kendı özel class larımız da 
+        //    staticleri kullanıyoruz neden kullandıgımızı da ayrı olarak
+        //    konusacagız
+
+        //    */
+
+        //}
+
+        #endregion
+
+
+
+
+
+    }
+}
