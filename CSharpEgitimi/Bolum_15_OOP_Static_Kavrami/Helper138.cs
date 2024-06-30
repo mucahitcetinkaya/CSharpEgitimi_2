f1ed4c8f2f3a48069c18e920c6ea60a60a83e334
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2024-06-30 20:46:50 +0300

    Bolum 15 OOP Static Kavrami V138 Static Metot Kullanımı

---
+++ CSharpEgitimi/Bolum_15_OOP_Static_Kavrami/Helper138.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+
+//namespace Bolum_15_OOP_Static_Kavrami
+//{
+//    public class Helper138
+//    {
+
+//        public static void emailGonder(string aliciEmailAdres, string konu, string icerik)
+//        {
+
+//            // email gonderme işlemleri devam edecek
+
+//            Console.WriteLine("EMail gönderim işlemi başarılı.");
+
+//        }
+
+
+
+
+//    }
+//}
+
+
+
+///*
+ 
+//1. not bitiş
+
+//2. not baslangıc
+
+//gerekli metotları yazalım 
+ 
+//Program.cs e gecelım 
+
+//2. not bitiş
+ 
+ 
+ 
+//*/
---
+++ CSharpEgitimi/Bolum_15_OOP_Static_Kavrami/Personel138.cs
@@ -0,0 +1,28 @@
+﻿//namespace Bolum_15_OOP_Static_Kavrami
+//{
+//    public class Personel138
+//    {
+
+//        public static string domainAdres = "firmam.com";
+//        public string Isim { get; set; }
+//        public string SoyIsim { get; set; }
+
+//        private string _emailAdres;
+//        public string EmailAdres
+//        {
+//            get
+//            {
+//                return this._emailAdres;
+//            }
+
+//            set
+//            {
+//                this._emailAdres = value.ToLower() + "@" + Personel138.domainAdres;
+//            }
+//        }
+
+//        public int Cinsiyet { get; set; }
+
+
+//    }
+//}
