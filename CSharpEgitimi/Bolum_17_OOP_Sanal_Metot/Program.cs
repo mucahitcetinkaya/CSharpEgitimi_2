d9d56653d8494823ed459c51affec270c16f0203
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2025-08-02 14:21:51 +0300

    Bolum 17 OOP Sanal Metot V151 Nesne yönelimli programlama dünyasında sanal metot nedir ?

---
+++ CSharpEgitimi/Bolum_17_OOP_Sanal_Metot/Program.cs
@@ -0,0 +1,56 @@
+﻿
+namespace Bolum_17_OOP_Sanal_Metot
+{
+    internal class Program
+    {
+        //#region VİDEO
+
+        //static void Main(string[] args)
+        //{
+
+
+        //}
+
+        //#endregion
+
+        #region VİDEO 151 Nesne yönelimli programlama dünyasında sanal metot nedir ?
+
+        //static void Main(string[] args)
+        //{
+        //    /*
+
+        //    Bolum 17 OOP Sanal Metot V151 Nesne yönelimli programlama dünyasında sanal metot nedir ?
+        //    Çok Biçimlilik Sanal Metot Kullanımı
+
+        //    Bir önceki bölümde kalıtım konusunu inceledik bunla alakalı aslında diğer bolumleri de kapsayan bir uygulama yapmıştık.
+
+        //    Şimdi çok biçimlilik konusunda sanal metot nedir ve polimorphszm içerisinde sanal metot nası kullanılır kısmını incelemeye çalışacagız
+
+        //    Sanal Metotlar
+
+        //    Bir önceki bölüm içerisinde kalıtım konusunu inceledik. Hatırlarsanız kalıtım içerisinde kalıtılan sınıf kendisini kalıtan sınıfa içerisindeki private metotlar haricinde bulunan tüm metotları kullanım izni sunmaktaydı.
+
+        //    Örnek olarak baseClass adında bulunan sınıfımızı urun sınıfı kalıttığı zaman baseClass içerisinde bulunan metotlara ve özelliklere erişebiliyor baseClass içerisindeki tanımları gibi kullanabiliyordu.
+
+        //    BaseClass içerisinde ekrana yaz adında bir metot olsa urun sınıfı içerisinde ekrana yaz metotu çağrıldığı zaman baseClass içerisinde bulunan şekli ile çalışıp metot işlemini tamamlayabiliyordu.
+
+        //    Sanal metot dediğimiz bu bölümümüz de baseclass içerisinde bulunan bir metotun istenilirse türeyen sınıf içerisinde nasıl yeniden şekillenebileceğini inceleyeceğiz.
+
+        //    Kalıtım konusunda tüm nesnelerin object nesnesinden kalıtıldığını ve bazı metotların object nesnesinden bize geldiğini söylemiştik.
+
+        //    Birazdan sanal metot anlatımına object içerisinde bize standart olarak gelen tostring metotunu inceleyerek başlayacagız
+
+        //    Sanal metotları X-men filminde bulunan mystique karakterine benzetebilirsiniz.
+
+        //    Ortama göre kendi şeklini değiştirebilen bir karakter olması buna örnek olabilir.
+
+        //    */
+
+        //}
+
+        #endregion
+
+
+
+    }
+}
