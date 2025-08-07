3f0ceb8dd05b5245a9b29552fd92ebe445ec5574
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2025-08-07 22:09:17 +0300

    Bolum 18 OOP Interface ve Abstract V157 Interface ve Abstract genel olarak nedir ?

---
+++ CSharpEgitimi/Bolum_18_OOP_Interface_Abstract/Program.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Bolum_18_OOP_Interface_Abstract
+{
+    internal class Program
+    {
+        //#region VİDEO
+
+        //static void Main(string[] args)
+        //{
+
+
+        //}
+
+        //#endregion
+
+
+        //#region VİDEO 157 Interface ve Abstract genel olarak nedir ?
+
+        //static void Main(string[] args)
+        //{
+
+        //    /*
+
+        //    Bolum 18 OOP Interface ve Abstract V157 Interface ve Abstract genel olarak nedir ?
+
+        //     */
+
+        //    /*
+
+        //    1. not başlangıç
+
+        //    Bu bölümde abstract ve interface nedir bunlar üzerinde biraz konuşuyor olacağız
+
+        //    Object orientent  konusunun artık sonlarına geldik
+
+        //    Bu bölümden sonra nesne yönelimli programlama kısmında bir çok şeyi anlamış ve kullanmış oluyoruz bunun sonrasında ise farklı bölümler ile aslında nesne konuları tekrar bizim karşımıza cıkacak
+
+        //    Bu bölüm ve bu bölümden sonra anlatılanlar artık ileri C# olarak size sunulacaktır
+
+        //    -------------
+
+        //    Abstract & Interface
+
+        //    C# nedir, neden kullanırız avantajları nelerdir.
+
+        //    Abstract & Interface
+
+        //    C# programlama dilinde aslında VIP dediğimiz alana geldik bu bölümde öğreneceğiniz konuların örnek kullanımları ve açıklamaları uygulama yaptığımız zaman net olarak anlaşılacaktır.
+        //    Bölüme başlamadan önce neden bu bölümden VIP olarak bahsetmiş olduğumu anlatayım.
+        //    Interface veya abstract ( Interface daha sık kullanılır ) aslında bizim uygulama içerisinde kullanmayacağımız ama her noktada varlığından faydalanacağımız tanımlardır.
+        //    Kurumsal projelerde kodlama yapacağımız zaman çok sık göreceğimiz bu kavramları kendi başımıza proje yaptığımız zaman ( tasarım deseni zorunlu değilse ) kullanmayız.
+        //    Fakat kurumsal projelerde bir ekibin kodlamasında görev alıyorsak sık kullandığımız tanımlardır.
+
+        //    Hatırlarsanız nesne yönelimli programlama konusunda girdiğimiz bölümlerden bu yana devamlı olarak nesnelerimizi oluşturduk farklı kullanımlar olsa bile bir şekilde nesnemizi örnekledik kullandık.
+        //    Nesnelerimizin içerisinde ise birçok tanım oluşturduk ve bu tanımları çağırıp kullandık.
+        //    Abstract ve Interface tanımlarında bir örnekleme söz konusu değildir.
+        //    Kendimize işe yapacak bir rol model çıkartırız. Nasıl olacağının kurallarını hazırlar ve nesnelerimiz de kurallarmızı kullanırız.
+        //    Bu bölümde oluşturmuş olduğumuz nesnelerimizi nasıl kurallar ile şartlayabileceğimizi konuşacağız.
+        //    Not : Bu bölümde anlatılan konular biraz havada kalabilir bu konuların sizin pratiğinizde oturabilmesi için bizim biraz ilerlememiz gerekir.
+
+
+
+
+        //     */
+
+        //}
+
+        //#endregion
+
+
+
+
+    }
+}
---
+++ CSharpEgitimi/Bolum_18_OOP_Interface_Abstract/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bolum_18_OOP_Interface_Abstract")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bolum_18_OOP_Interface_Abstract")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b3ec84c-d1a4-4c2a-bf15-a8138062a7dc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
