533bf3f5bdfdcb76ef25f3b48af9309058a22146
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2025-08-10 14:43:27 +0300

    Bolum 19 Partial Class V163 Bilgilendirme

---
+++ CSharpEgitimi/Bolum_19_Partial_Class/Program.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Bolum_19_Partial_Class
+{
+    internal class Program
+    {
+        //#region VİDEO
+
+        //static void Main(string[] args)
+        //{
+
+
+        //}
+
+        //#endregion
+
+        #region VİDEO 163 Bilgilendirme
+
+        //static void Main(string[] args)
+        //{
+
+        //    /*
+
+        //        Bolum 19 Partial Class V163 Bilgilendirme
+
+
+        //    */
+
+        //    /*
+
+        //    1. not baslangıc
+
+        //    bu bölümde bilmemizin iyi olacagı bilmesekte calısmamıza engel olmayacagı bir kavram olan partial class ları görecegız 
+
+        //    class ları biz class musteri vs gibi yazıyorduk bu class ın içinde de problar fiel lar metotlar vs oluyordu 
+
+        //    bunu uzun süreçte düşündüğünüz de bir uygulama yazarken birden fazla baglantı kuracagımız için bir sürü kod blogumuz olur 
+
+        //    be ilgili metotu aratmak zorunda kalırız ctrl + f yani bu kodda bir duzensizliğe de yol acmakta 
+
+        //    şöyle bir şey istersek ne olur
+
+        //    benım öyle bir mantıgım olsun ki 
+        //    ben musteri class ımın field larını bir yerde toplayayım 
+        //    ben musteri class ımın metot larını bir yerde toplayayım 
+
+        //    eger field larına bakmak istersem ilgili bölümdeki alana bakıp field larını arayabileyim yada ekleyebileyim 
+        //    metotlarına bakmak istersem de farklı bir alandan gideyim metotlarını göreyim 
+        //    fakat uygulama calıstıgı zaman 2 farklı parcayı tek bir parca olarak elde edebileyim 
+
+        //    n katmanli mimari repostory disagne pattern vb database ilişkilerin de kullandıgımız business servisler de partial class ların onemini daha net anlayacagız 
+
+        //    diger derse gecelim.
+
+
+        //    */
+
+
+        //}
+
+        #endregion
+
+
+    }
+}
---
+++ CSharpEgitimi/Bolum_19_Partial_Class/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bolum_19_Partial_Class")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bolum_19_Partial_Class")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a48be54-afac-46a5-a783-d41a40ebd4dd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
