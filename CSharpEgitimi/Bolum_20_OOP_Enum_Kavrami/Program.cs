4da09cc3512241b6deac9a695afc0777125c3609
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2025-08-10 16:00:01 +0300

    Bolum 20 OOP Enum Kavrami V166 Bilgilendirme

---
+++ CSharpEgitimi/Bolum_20_OOP_Enum_Kavrami/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Bolum_20_OOP_Enum_Kavrami
+{
+    internal class Program
+    {
+        //#region VİDEO
+
+        //static void Main(string[] args)
+        //{
+
+
+        //}
+
+        //#endregion
+
+        #region VİDEO 166 Bilgilendirme
+
+        //static void Main(string[] args)
+        //{
+
+        //    /*
+
+        //        Bolum 20 OOP Enum Kavrami V166 Bilgilendirme
+
+
+        //    */
+
+        //    /*
+            
+        //    1. not baslangıc
+
+        //    uygulamalarımızı yazarken bize avantaj saglayacak
+        //    onceki ve sonraki yazılımcıya da avantaj saglayacak dua ettirecek bir konudan bahsedeceğiz enum lar 
+
+        //    okuma kolaylıgı acısından karsısına cıkacaktır ilgili kod satırını daha kolay yorumlayabilecektir
+
+        //    enum lara neden ihtiyac duyarız
+        //    enum nedir
+        //    ne gibi avantaj saglar
+        //    yazdıgımız uygulamaya avantaj saglar mı 
+        //    olmasada bu iş olur mu 
+        //    olması ne kazandırır
+                         
+        //    */
+          
+        //}
+
+        #endregion
+
+
+    }
+}
