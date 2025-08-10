112a59df51710ec6f2de254defa4556b8f7b659d
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2025-08-10 16:20:27 +0300

    Bolum 20 OOP Enum Kavrami V167 Enum Nedir

--- CSharpEgitimi/Bolum_20_OOP_Enum_Kavrami/Program.cs
+++ CSharpEgitimi/Bolum_20_OOP_Enum_Kavrami/Program.cs
@@ -26,7 +26,7 @@ namespace Bolum_20_OOP_Enum_Kavrami
         //    */
 
         //    /*
-            
+
         //    1. not baslangıc
 
         //    uygulamalarımızı yazarken bize avantaj saglayacak
@@ -40,13 +40,108 @@ namespace Bolum_20_OOP_Enum_Kavrami
         //    yazdıgımız uygulamaya avantaj saglar mı 
         //    olmasada bu iş olur mu 
         //    olması ne kazandırır
-                         
+
         //    */
-          
+
         //}
 
         #endregion
 
+        #region VİDEO 167 Enum Nedir
+
+        //static void Main(string[] args)
+        //{
+
+        //    /*
+
+        //        Bolum 20 OOP Enum Kavrami V167 Enum Nedir
+
+
+        //    */
+
+        //    /*
+
+        //    1. not baslangıc
+
+        //    eski kodumuz da olabilir baska birinden de kod almıs olabiliriz
+
+        //    katmanlı mimari uygulanmıyorsa if else blokları gelecektir
+
+        //    ogrenci nesnesini bolum 19 dan ogrenci nesnesinden aldık hoca burada anlatmadı eski kod veya baskasından kod aldıgımız için kendı yazmıs anlatmak için boyle olusturmus akıllı adam
+
+        //    Ogrenci nesnesi olusturalım
+
+        //    1. not bitiş
+
+        //    2. not bitiş ogrenciden geldik
+
+        //    3. not baslangıc
+
+        //    ogrenci nesnesini bolum 19 dan ogrenci nesnesinden aldık hoca burada anlatmadı
+
+        //    V167Ogrenci O1 = new V167Ogrenci();
+        //    O1.id = 1;
+        //    int ogrenciKayitSonuc = O1.yeniKayit(O1);
+
+        //    new ledik ve prop ve metot larını kullandık
+
+        //    if else lerini yazalım
+
+        //    ogrenci tipinden bir deger aktarımı yapılmıs ogrenciyi sisteme kayıt yapıyorum 
+        //    kayıt etmeye calısırken ilgili metot bana bir sekılde bir metot donuyor 
+        //    donmus oldugu deger ogrenciKayitSonuc degeri 
+        //    bana burdan bir int deger dondu bu degeri benım yönlendirmem lazım 
+        //    kayıt oldu mu
+        //    parametresi dogru mu 
+        //    runtime da hata mı aldım
+        //    bütün soruların cevabında 71777700x ile donen bir deger var 0 a dogru kayıt 1 de parametresi dogru yanlıs vs gibi
+
+        //    bunların acıklamasını kim biliyor kim yazdıysa o biliyor 
+        //    uygulamayı kodlarken biz biliyoduk 
+        //    ama şimdi hangisi ne bilmiyoruz 
+
+        //    bunu anlamak için yenikayıtmetot içine gidip yazdıgımız veya yazılmıs kod bloklarını tek tek yenıden debug ederek uzerlerınden gecip hata kodlarının acıklamasını hatırlayacagız veya ogrenecegız
+        //    işte bu karmasadan bizi kurtaran bir yapıdır enum 
+
+        //    nesne yonelımlı kod yazma veya yazarken içerisindeki bütün degerleri dogru yerlerde kullanabilme eger firmanızın bir kuralı olduysa hiç bir kodu devir almaktan korkmayın 
+        //    ama herkesın kafasına göre kod yazmıs oldugu yerlerde ise giden arkadası biraz yorunki karsınıza boyle bişey cıktıgında onları olması gereken yapıya cok kolay cevirebilirsiniz
+
+        //    şimdı böyle bir karısık yapıyı daha okunaklı nasıl yaparız bunu birlikte ogrenelim
+
+        //    */
+
+        //    V167Ogrenci O1 = new V167Ogrenci();
+        //    O1.id = 1;
+        //    int ogrenciKayitSonuc = O1.yeniKayit(O1);
+
+        //    if (ogrenciKayitSonuc == 717777000)
+        //    {
+
+        //    }
+        //    else if (ogrenciKayitSonuc == 717777001)
+        //    {
+
+        //    }
+        //    else if (ogrenciKayitSonuc == 717777002)
+        //    {
+
+        //    }
+        //    else if (ogrenciKayitSonuc == 717777003)
+        //    {
+
+        //    }
+        //    else if (ogrenciKayitSonuc == 717777004)
+        //    {
+
+        //    }
+        //    else
+        //    {
+
+        //    }
+
+        //}
+
+        #endregion
 
     }
 }
---
+++ CSharpEgitimi/Bolum_20_OOP_Enum_Kavrami/V167Ogrenci.cs
@@ -0,0 +1,45 @@
+﻿
+//using System;
+
+//namespace Bolum_20_OOP_Enum_Kavrami
+//{
+//    public class V167Ogrenci
+//    {
+//        public int id { get; set; }
+//        public string isim { get; set; }
+//        public string soyisim { get; set; }
+//        public string ogrenciNumarasi { get; set; }
+
+//        public int yeniKayit(V167Ogrenci m)
+//        {
+//            Console.WriteLine("musteri yeniKayit");
+//            return 1;
+//        }
+
+//        public int kayitGuncelle(V167Ogrenci m)
+//        {
+//            Console.WriteLine("musteri kayitGuncelle");
+//            return 1;
+//        }
+
+//        public int kayitSil(int id)
+//        {
+//            Console.WriteLine("musteri kayitSil");
+//            return 1;
+//        }
+
+
+//    }
+//}
+
+///*
+ 
+//1. not bitiş program.cs den geldik
+
+//2. not baslangıc
+
+//hoca bunları anlatmıyor bolum 19 dan aldım ogrenci nesnesinden
+ 
+//2. not bitiş program.cs e gidelim
+ 
+//*/
